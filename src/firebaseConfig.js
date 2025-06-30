b6f43a4e04eccfe322e9f1c8f7dbb3501001a6f2
Vamsireddy18 <126796957+krishnavamsireddy03@users.noreply.github.com>
2025-06-30 19:27:15 +0530

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAtddOy55mHf_nSJ2TWt1z5X1sBtABtVf8",
+  authDomain: "ecommerce-dashboard-b2a70.firebaseapp.com",
+  projectId: "ecommerce-dashboard-b2a70",
+  storageBucket: "ecommerce-dashboard-b2a70.firebasestorage.app",
+  messagingSenderId: "289714071684",
+  appId: "1:289714071684:web:9960cb335dfcdd7ff8fad1",
+  measurementId: "G-Y2CJ06987C"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
