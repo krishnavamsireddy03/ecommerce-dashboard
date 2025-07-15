6098608132b3fdf18d6f1280acdeda47bb6aeaf0
Vamsireddy18 <126796957+krishnavamsireddy03@users.noreply.github.com>
2025-07-15 12:41:18 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// added these lines
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"; 
 // TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -19,3 +23,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+
+//changes
+const auth = getAuth(app);
+const db = getFirestore(app); 
+export { auth, db };
